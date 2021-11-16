7e439d6d8ab1cae47c92a9df885c01ead84966e0
Devin-M-Smith <91283760+Devin-M-Smith@users.noreply.github.com>
2021-11-16 14:31:56 -0500

    Update home screen

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import './home.css'
 function home() {
     return (
-        <div>
-            <h1>HomePage</h1>
+        <div id='home'>
+            
         </div>
     )
 }
