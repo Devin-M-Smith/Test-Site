d291da212a11c855ffea2ee5a45c8ea21c8e411f
Devin-M-Smith <91283760+Devin-M-Smith@users.noreply.github.com>
2021-12-09 14:25:26 -0500

    Update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import './App.css';
 import Navbar from "./components/Navbar";
-import {Switch, Route } from "react-router-dom";
+import { Route } from "react-router-dom";
 import Home from './pages/home'
 import About from './pages/about'
 import Contact from './pages/contact'
